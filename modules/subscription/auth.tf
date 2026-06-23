c9c3277992f566405aa0da74864b5e5e7cfef59d
davidagnew-sys <david.agnew@upwind.io>
2026-06-23 11:32:27 +0100

    fix(UP-2418): add the ap region description and validation

--- modules/subscription/auth.tf
+++ modules/subscription/auth.tf
@@ -10,6 +10,7 @@ data "http" "upwind_get_access_token_request" {
     var.upwind_region == "us" ? var.upwind_auth_endpoint :
     var.upwind_region == "eu" ? replace(var.upwind_auth_endpoint, ".upwind.", ".eu.upwind.") :
     var.upwind_region == "me" ? replace(var.upwind_auth_endpoint, ".upwind.", ".me.upwind.") :
+    var.upwind_region == "ap" ? replace(var.upwind_auth_endpoint, ".upwind.", ".me.upwind.") :
     var.upwind_auth_endpoint
   )
 
@@ -20,6 +21,7 @@ data "http" "upwind_get_access_token_request" {
   request_body = join("&", [
     "grant_type=client_credentials",
     format("audience=%s", var.upwind_region == "us" ? var.upwind_integration_endpoint :
+      var.upwind_region == "eu" ? replace(var.upwind_integration_endpoint, ".upwind.", ".eu.upwind.") :
       var.upwind_region == "eu" ? replace(var.upwind_integration_endpoint, ".upwind.", ".eu.upwind.") :
       var.upwind_region == "me" ? replace(var.upwind_integration_endpoint, ".upwind.", ".me.upwind.") :
       var.upwind_integration_endpoint
