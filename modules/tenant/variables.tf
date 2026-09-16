8d5227904589524baee0c5d12e17a6077378e9fc
Michael Vaisnoras <michael.vaisnoras@upwind.io>
2026-09-16 11:54:26 +0100

    feat: accept the pdc02 region for upwind_region

--- modules/tenant/variables.tf
+++ modules/tenant/variables.tf
@@ -64,7 +64,7 @@ variable "upwind_region" {
   default     = "us"
 
   validation {
-    condition     = var.upwind_region == "us" || var.upwind_region == "eu" || var.upwind_region == "me" || var.upwind_region == "pdc01" || var.upwind_region == "ap"
+    condition     = var.upwind_region == "us" || var.upwind_region == "eu" || var.upwind_region == "me" || var.upwind_region == "pdc01" || var.upwind_region == "ap" || var.upwind_region == "pdc02"
     error_message = "upwind_region must be either 'us', 'eu', 'ap' or 'me'."
   }
 }
